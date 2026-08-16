6a8217d5931264f6985b8e2b708424436bcd9ca3
HaileyKernan <hailey.a.kernan@gmail.com>
2026-08-16 09:23:59 -0700

    Updated frontend/app.py

--- frontend/app.py
+++ frontend/app.py
@@ -27,7 +27,7 @@ product_type = st.selectbox('Product Type', ['Fruits and Vegetables',
                                              'Breakfast',
                                              'Seafood'])
 product_mrp = st.number_input('Product MRP', min_value=0.0, value=0.0)
-store_establishment_year = st.number_input('Store Establishment Year', min_value=0, value=0)
+store_establishment_year = st.selectbox('Store Establishment Year', ['1897', '1998', '1999', '2009'])
 store_location_city_type = st.selectbox('Store Location City Type', ['Tier 1', 'Tier 2', 'Tier 3'])
 store_type = st.selectbox('Store Type', ['Supermarket Type2', 'Supermarket Type1 ', 'Departmental Store','Food Mart'])
 store_size = st.selectbox('Store Size', ['Small', 'Medium', 'High'])
@@ -44,11 +44,11 @@ user_input = pd.DataFrame([{
     'Store_Type':store_type
 }])
 
-if st.button('Predict', type='primary'):
+if st.button('Predict', type='primary', key='single'):
   response = requests.post(f'{backend_url}/v1/singleprediction', json=user_input.to_dict(orient='records')[0])
   if response.status_code == 200:
     prediction = response.json()['Forcast']
-    st.success(f'The sales forcast for this item is is: ${prediction}')
+    st.success(f'The sales forcast for this item is is: ${prediction:.2f}')
   else:
     st.error('Error occured during prediction')
 
@@ -57,7 +57,7 @@ st.subheader("Batch Forcast")
 upload_file = st.file_uploader('Upload a CSV file', type=['csv'])
 
 if upload_file is not None:
-  if st.button('Predict', type='primary'):
+  if st.button('Predict', type='primary', key = 'batch'):
     response = requests.post(f'{backend_url}/v1/batchprediction', files={'files':upload_file})
     if response.status_code == 200:
       prediction_dict = response.json()
